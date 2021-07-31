6be5ff53ae6b925d12ea18e502a96957e9774c8c
muskaanshah <hmuskaan11@gmail.com>
2021-08-01 01:33:20 +0530

    logic completed

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 var btnNext = document.getElementById("next");
-var opBill = document.getElementById("outputBill");
+var errorBill = document.getElementById("outputBill");
 var billamt = document.getElementById("bill");
 var btnCheck = document.getElementById("check");
 var cashamt = document.getElementById("cash");
-var opChk = document.getElementById("outputCheck");
-
+var errorCheck = document.getElementById("outputCheck");
+var tablereturnchange = document.querySelector("table");
+var noofnotes = document.querySelectorAll(".noofnotes");
 
 btnNext.addEventListener("click", validate1);
 btnCheck.addEventListener("click", validate2);
 
+var notesavailable = [2000, 500, 100, 20, 10, 5, 1];
+
 function validate1() {
-    const bills = billamt.value;
-    if (bills <=0)
-        opBill.innerText = "Enter valid bill amount";
+    errorBill.style.display = "none";
+    if (billamt.value <=0) {
+        errorBill.innerText = "Enter valid bill amount";
+        errorBill.style.display = "block";
+    }
     else {
         document.getElementById("checkDiv").style.display = "block";
-        opBill.style.display = "none";
     }  
 }
 
 function validate2() {
-    const chkBill = billamt.value;
-    const chkCash = cashamt.value;
-    if (chkCash < chkBill) 
-        opChk.innerText = "Sorry! Cash given lesser than bill amount";
-    else if (chkCash <=0 || chkBill <=0 )
-        opChk.innerText = "Sorry! Invalid cash or bill amount";
-    else 
-        console.log("Checking");
+    tablereturnchange.style.display = "none";
+    errorCheck.style.display = "block";
+    if (cashamt.value < billamt.value) {
+        errorCheck.innerText = "Sorry! Cash given lesser than bill amount";
+    }
+    else if (cashamt.value <=0 || billamt.value <=0 )
+    {
+        errorCheck.innerText = "Sorry! Invalid cash or bill amount";
+    }
+    else if (cashamt.value == billamt.value)
+    {
+        errorCheck.innerText = "No change to be returned";
+    }
+    else {
+        errorCheck.style.display = "none";
+        tablereturnchange.style.display = "inline-table";
+        changetoreturn = cashamt.value - billamt.value;
+        amountreturned(changetoreturn);
+    }
+}
+
+function amountreturned (change) {
+    for (i=0; i<notesavailable.length; i++) {
+        var note = Math.floor(change/notesavailable[i]);
+        noofnotes[i].innerText = note;
+        var change = change % notesavailable[i];
+    }
+
 }
